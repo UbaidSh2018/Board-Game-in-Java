0bd7fe777d80b9b77446e28b5a50cb427d8e2cd5
ubaid <ubaidshowkat91@gmail.com>
2019-01-02 16:18:04 +1100

    Pushing:
    1. Added functionality of accepting arguments from command line line(args).
    2. Removed standard input

--- src/ToyRobot/Main.java
+++ src/ToyRobot/Main.java
@@ -1,14 +1,6 @@
 package ToyRobot;
 
 import java.io.*;
-import java.nio.Buffer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
 
@@ -19,29 +11,31 @@ public class Main {
 
         GameBoard board = new GameBoard(BOARD_HEIGHT, BOARD_WIDTH);
         Robot robot = null;
-        Direction direction;
+        File file;
 
-        Scanner sc = new Scanner(System.in);
+   /*     Scanner sc = new Scanner(System.in);
         System.out.println("Enter File Name: ");
         String filePassed = sc.nextLine();
-        File file = new File(filePassed);
+        File file = new File(filePassed);*/
 
-      //  File file = new File("CommandFiles/InvalidPlace.txt");
-
-        if(file.exists()){
-            System.out.println("file exists");
-        FileReader fileReader;
-            try {
-                fileReader = new FileReader(file);
-                BufferedReader bufferedReader = new BufferedReader(fileReader);
-                String singleCommandLine;
-                int fileLines = 0;
-                System.out.println("file buffered");
+        //  File file = new File("CommandFiles/InvalidPlace.txt");
+        if (0 < args.length) {
+            file = new File(args[0]);
+            System.out.println("file passed in args");
+            if (file.exists()) {
+                System.out.println("file exists");
+                FileReader fileReader;
+                try {
+                    fileReader = new FileReader(file);
+                    BufferedReader bufferedReader = new BufferedReader(fileReader);
+                    String singleCommandLine;
+                    int fileLines = 0;
+                    System.out.println("file buffered");
 
                     try {
                         while ((singleCommandLine = bufferedReader.readLine()) != null) {
                             System.out.println("reading line");
-                            if(singleCommandLine.toUpperCase().startsWith("PLACE")){
+                            if (singleCommandLine.toUpperCase().startsWith("PLACE")) {
                                 System.out.println("place command found");
                                 String[] firstLine = singleCommandLine.split(" ");
                                 String[] getParams = firstLine[1].split(",");
@@ -50,7 +44,7 @@ public class Main {
                                     System.out.println("valid place command");
                                     int x = Integer.parseInt(getParams[0]);
                                     int y = Integer.parseInt(getParams[1]);
-                                    direction = Direction.valueOf(getParams[2]);
+                                    Direction direction = Direction.valueOf(getParams[2]);
 
                                     if ((x >= 0 && x <= board.getBoardHeight() &&
                                             (y >= 0 && y <= board.getBoardWidth()))) {
@@ -58,25 +52,26 @@ public class Main {
                                         robot = new Robot(x, y, direction);
                                         robot.loadBoard(board);
                                     }
-                                }else System.out.println("PLACE Command Invalid. Correct Format \"PLACE X,Y,Direction\" ");
+                                } else
+                                    System.out.println("PLACE Command Invalid. Correct Format \"PLACE X,Y,Direction\" ");
 
                             }
 
-                            if(robot !=null){
+                            if (robot != null) {
                                 System.out.println("Robot object not null");
-                               if(singleCommandLine.equalsIgnoreCase("MOVE")){
-                                   robot.move();
-                                   System.out.println("move executed");
-                               } else if(singleCommandLine.equalsIgnoreCase("RIGHT")){
-                                   robot.rotateRight();
-                                   System.out.println("rotate right executed");
-                               } else if(singleCommandLine.equalsIgnoreCase("LEFT")){
-                                   robot.rotateLeft();
-                                   System.out.println("rotate left executed");
-                               } else if(singleCommandLine.equalsIgnoreCase("REPORT")){
-                                   System.out.println(robot.report());
-                                   System.out.println("report executed");
-                               }
+                                if (singleCommandLine.equalsIgnoreCase("MOVE")) {
+                                    robot.move();
+                                    System.out.println("move executed");
+                                } else if (singleCommandLine.equalsIgnoreCase("RIGHT")) {
+                                    robot.rotateRight();
+                                    System.out.println("rotate right executed");
+                                } else if (singleCommandLine.equalsIgnoreCase("LEFT")) {
+                                    robot.rotateLeft();
+                                    System.out.println("rotate left executed");
+                                } else if (singleCommandLine.equalsIgnoreCase("REPORT")) {
+                                    System.out.println(robot.report());
+                                    System.out.println("report executed");
+                                }
                             }
 
                             fileLines++;
@@ -85,33 +80,35 @@ public class Main {
 
                         //....Check for Empty File....//
                         if (fileLines == 0) {
-                            System.out.println(" : is an Empty File");
+                            System.out.println(args[0] + " is an Empty File");
                         }
                         //....Check for Invalid Command File if it fails to instantiate RobotCommands Class....//
                         if (robot == null) {
-                                System.out.println(" : is an Invalid Command File");
-                            }
-
+                            System.out.println(args[0] + " is an Invalid Command File");
+                        }
 
                         bufferedReader.close();
 
                     } catch (IOException e) {
+                        System.out.println("IO Exception Thrown");
                         e.printStackTrace();
                     }
 
 
+                } catch (FileNotFoundException e) {
+                    System.out.println(args[0] +" File Not Found Exception Thrown");
+                    e.printStackTrace();
 
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+                }
 
+            } else {
+                System.out.println(args[0] + " - Invalid File Name or Path. Try Again");
             }
 
-
-
-        } else{
-            System.out.println("Invalid File Name or Path: Try Again");
+        } else {
+            System.err.println("Invalid arguments count: " + args.length);
+            System.exit(1);
         }
-
     }
 
 }
