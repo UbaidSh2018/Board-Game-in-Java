dd0283b00d19be2606d4c13fe693753cec77868f
ubaid <ubaidshowkat91@gmail.com>
2019-01-04 20:08:38 +1100

    Pushing:
    1. Added Test Suite class

---
+++ TestToyRobot/ToyRobot/RunAllTests.java
@@ -0,0 +1,14 @@
+package ToyRobot;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        GameBoardTest.class,
+        MainTest.class
+})
+public class RunAllTests {
+
+
+}
