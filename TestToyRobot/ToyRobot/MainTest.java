bdfcb5befb093604bff819ff16579855857a8606
ubaid <ubaidshowkat91@gmail.com>
2019-01-04 17:06:24 +1100

    Pushing:
    1. Added Tests for Main function till Invalid report command file

--- TestToyRobot/ToyRobot/MainTest.java
+++ TestToyRobot/ToyRobot/MainTest.java
@@ -35,7 +35,7 @@ public class MainTest {
     }
 
     @Test
-    public void testInvalidPlaceInput() throws IOException{
+    public void testInvalidPlace() throws IOException{
         String[] args = {"CommandFiles/C2_InvalidPlace.txt"};
         String output;
 
@@ -53,9 +53,46 @@ public class MainTest {
         assertEquals("PLACE Command Invalid. Correct Format PLACE X,Y,Direction", breakDownOutput[0]);
         assertEquals("CommandFiles/C2_InvalidPlace.txt is an Invalid Command File", breakDownOutput[1]);
 
+    }
 
+    @Test
+    public void testInvalidMovementCommand() throws IOException {
+        String[] args = {"CommandFiles/C3_InvalidMovementCommands.txt"};
+        String output;
 
+        try (ByteArrayOutputStream bOutput = new ByteArrayOutputStream()) {
+            System.setOut(new PrintStream(bOutput));
+            Main.main(args);
+            bOutput.flush();
+            output = new String(bOutput.toString());
+        }
+        String newLine = System.getProperty("line.separator");
+        String[] breakDownOutput = output.split(newLine);
+
+        assertEquals(1, breakDownOutput.length);
+        assertEquals("Output:(X, Y , Direction)= (2,2,NORTH)" , breakDownOutput[0]);
 
     }
+
+    @Test
+    public void testInvalidReportCommand() throws IOException{
+        String[] args = {"CommandFiles/C4_InvalidReportCommand.txt"};
+        String output;
+
+        try (ByteArrayOutputStream bOutput = new ByteArrayOutputStream()) {
+            System.setOut(new PrintStream(bOutput));
+            Main.main(args);
+            bOutput.flush();
+            output = new String(bOutput.toString());
+        }
+
+        String newLine = System.getProperty("line.separator");
+        String[] breakDownOutput = output.split(newLine);
+
+        assertEquals(1, breakDownOutput.length);
+        assertEquals("", breakDownOutput[0]);
+    }
+
+
 
 }
