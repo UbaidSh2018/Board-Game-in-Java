4bcc5a28190054a4bc28ee2a8c543bc4d6fb8e75
ubaid <ubaidshowkat91@gmail.com>
2019-01-04 18:54:56 +1100

    Pushing:
    1. Added Tests for Main function till Western Border check command file

--- TestToyRobot/ToyRobot/MainTest.java
+++ TestToyRobot/ToyRobot/MainTest.java
@@ -6,12 +6,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 
-import static javafx.scene.input.KeyCode.X;
-import static javafx.scene.input.KeyCode.Y;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MainTest {
-
 
 
     @Test
@@ -23,7 +20,7 @@ public class MainTest {
             System.setOut(new PrintStream(bOutput));
             Main.main(args);
             bOutput.flush();
-            output = new String(bOutput.toString());
+            output = bOutput.toString();
         }
         String newLine = System.getProperty("line.separator");
         String[] breakDownOutput = output.split(newLine);
@@ -31,11 +28,11 @@ public class MainTest {
         assertEquals(2, breakDownOutput.length);
         assertEquals("CommandFiles/C1_EmptyFile.txt is an Empty File", breakDownOutput[0]);
         assertEquals("CommandFiles/C1_EmptyFile.txt is an Invalid Command File", breakDownOutput[1]);
-       // assertEquals("Empty file: testFiles/TestCase_04_EmptyInputFile.txt", linesOfOutput[0]);
+        // assertEquals("Empty file: testFiles/TestCase_04_EmptyInputFile.txt", linesOfOutput[0]);
     }
 
     @Test
-    public void testInvalidPlace() throws IOException{
+    public void testInvalidPlace() throws IOException {
         String[] args = {"CommandFiles/C2_InvalidPlace.txt"};
         String output;
 
@@ -43,7 +40,7 @@ public class MainTest {
             System.setOut(new PrintStream(bOutput));
             Main.main(args);
             bOutput.flush();
-            output = new String(bOutput.toString());
+            output = bOutput.toString();
         }
 
         String newLine = System.getProperty("line.separator");
@@ -64,18 +61,18 @@ public class MainTest {
             System.setOut(new PrintStream(bOutput));
             Main.main(args);
             bOutput.flush();
-            output = new String(bOutput.toString());
+            output = bOutput.toString();
         }
         String newLine = System.getProperty("line.separator");
         String[] breakDownOutput = output.split(newLine);
 
         assertEquals(1, breakDownOutput.length);
-        assertEquals("Output:(X, Y , Direction)= (2,2,NORTH)" , breakDownOutput[0]);
+        assertEquals("Output:(X, Y , Direction)= (2,2,NORTH)", breakDownOutput[0]);
 
     }
 
     @Test
-    public void testInvalidReportCommand() throws IOException{
+    public void testInvalidReportCommand() throws IOException {
         String[] args = {"CommandFiles/C4_InvalidReportCommand.txt"};
         String output;
 
@@ -83,7 +80,7 @@ public class MainTest {
             System.setOut(new PrintStream(bOutput));
             Main.main(args);
             bOutput.flush();
-            output = new String(bOutput.toString());
+            output = bOutput.toString();
         }
 
         String newLine = System.getProperty("line.separator");
@@ -93,6 +90,198 @@ public class MainTest {
         assertEquals("", breakDownOutput[0]);
     }
 
+    @Test
+    public void testFirstCommandNotPlace() throws IOException {
+        String[] args = {"CommandFiles/C5_FirstCommandNotPlace.txt"};
+        String output;
 
+        try (ByteArrayOutputStream bOutput = new ByteArrayOutputStream()) {
+            System.setOut(new PrintStream(bOutput));
+            Main.main(args);
+            bOutput.flush();
+            output = bOutput.toString();
+        }
+
+        String newLine = System.getProperty("line.separator");
+        String[] breakDownOutput = output.split(newLine);
+
+        assertEquals(2, breakDownOutput.length);
+        assertEquals("Output:(X, Y , Direction)= (0,0,EAST)", breakDownOutput[0]);
+        assertEquals("Output:(X, Y , Direction)= (0,0,NORTH)", breakDownOutput[1]);
+    }
+
+    @Test
+    public void testLeftRotateCommand() throws IOException {
+        String[] args = {"CommandFiles/C6_LeftRotateCommand.txt"};
+        String output;
+
+        try (ByteArrayOutputStream bOutput = new ByteArrayOutputStream()) {
+            System.setOut(new PrintStream(bOutput));
+            bOutput.flush();
+            Main.main(args);
+            output = bOutput.toString();
+        }
+
+        String newLine = System.getProperty("line.separator");
+        String[] breakDownOutput = output.split(newLine);
+
+        assertEquals(1, breakDownOutput.length);
+        assertEquals("Output:(X, Y , Direction)= (0,0,WEST)", breakDownOutput[0]);
+
+    }
+
+    @Test
+    public void testRightRotateCommand() throws IOException {
+        String[] args = {"CommandFiles/C7_RightRotateCommand.txt"};
+        String output;
+
+        try (ByteArrayOutputStream bOutput = new ByteArrayOutputStream()) {
+            System.setOut(new PrintStream(bOutput));
+            bOutput.flush();
+            Main.main(args);
+            output = bOutput.toString();
+        }
+
+        String newLine = System.getProperty("line.separator");
+        String[] breakDownOutput = output.split(newLine);
+
+        assertEquals(1, breakDownOutput.length);
+        assertEquals("Output:(X, Y , Direction)= (0,0,EAST)", breakDownOutput[0]);
+
+    }
+
+    @Test
+    public void testValidComboCommand() throws IOException {
+        String[] args = {"CommandFiles/C8_ValidComboCommands.txt"};
+        String output;
+
+        try (ByteArrayOutputStream bOutput = new ByteArrayOutputStream()) {
+            System.setOut(new PrintStream(bOutput));
+            bOutput.flush();
+            Main.main(args);
+            output = bOutput.toString();
+        }
+
+        String newLine = System.getProperty("line.separator");
+        String[] breakDownOutput = output.split(newLine);
+
+        assertEquals(4, breakDownOutput.length);
+        assertEquals("Output:(X, Y , Direction)= (0,4,EAST)", breakDownOutput[0]);
+        assertEquals("Output:(X, Y , Direction)= (3,4,NORTH)", breakDownOutput[1]);
+        assertEquals("Output:(X, Y , Direction)= (4,4,NORTH)", breakDownOutput[2]);
+        assertEquals("Output:(X, Y , Direction)= (4,3,WEST)", breakDownOutput[3]);
+
+
+    }
+
+    @Test
+    public void testMultiplePlaceCommand() throws IOException {
+        String[] args = {"CommandFiles/C9_MultiplePlaceCommands.txt"};
+        String output;
+
+        try (ByteArrayOutputStream bOutput = new ByteArrayOutputStream()) {
+            System.setOut(new PrintStream(bOutput));
+            bOutput.flush();
+            Main.main(args);
+            output = bOutput.toString();
+        }
+
+        String newLine = System.getProperty("line.separator");
+        String[] breakDownOutput = output.split(newLine);
+
+        assertEquals(3, breakDownOutput.length);
+        assertEquals("Output:(X, Y , Direction)= (2,0,NORTH)", breakDownOutput[0]);
+        assertEquals("Output:(X, Y , Direction)= (0,0,EAST)", breakDownOutput[1]);
+        assertEquals("Output:(X, Y , Direction)= (4,2,NORTH)", breakDownOutput[2]);
+
+
+    }
+
+    @Test
+    public void testNorthBorder() throws IOException {
+        String[] args = {"CommandFiles/C10_NorthBorderCheck.txt"};
+        String output;
+
+        try (ByteArrayOutputStream bOutput = new ByteArrayOutputStream()) {
+            System.setOut(new PrintStream(bOutput));
+            bOutput.flush();
+            Main.main(args);
+            output = bOutput.toString();
+        }
+
+        String newLine = System.getProperty("line.separator");
+        String[] breakDownOutput = output.split(newLine);
+
+        assertEquals(3, breakDownOutput.length);
+        assertEquals("You've reached Northern Border : Can't go beyond this point", breakDownOutput[0]);
+        assertEquals("You've reached Northern Border : Can't go beyond this point", breakDownOutput[1]);
+        assertEquals("Output:(X, Y , Direction)= (4,2,EAST)", breakDownOutput[2]);
+
+    }
+
+    @Test
+    public void testSouthBorder() throws IOException {
+        String[] args = {"CommandFiles/C11_SouthBorderCheck.txt"};
+        String output;
+
+        try (ByteArrayOutputStream bOutput = new ByteArrayOutputStream()) {
+            System.setOut(new PrintStream(bOutput));
+            bOutput.flush();
+            Main.main(args);
+            output = bOutput.toString();
+        }
+
+        String newLine = System.getProperty("line.separator");
+        String[] breakDownOutput = output.split(newLine);
+
+        assertEquals(3, breakDownOutput.length);
+        assertEquals("You've reached Southern Border : Can't go beyond this point", breakDownOutput[0]);
+        assertEquals("You've reached Southern Border : Can't go beyond this point", breakDownOutput[1]);
+        assertEquals("Output:(X, Y , Direction)= (0,1,EAST)", breakDownOutput[2]);
+
+    }
+
+    @Test
+    public void testEastBorder() throws IOException {
+        String[] args = {"CommandFiles/C12_EastBorderCheck.txt"};
+        String output;
+
+        try (ByteArrayOutputStream bOutput = new ByteArrayOutputStream()) {
+            System.setOut(new PrintStream(bOutput));
+            bOutput.flush();
+            Main.main(args);
+            output = bOutput.toString();
+        }
+
+        String newLine = System.getProperty("line.separator");
+        String[] breakDownOutput = output.split(newLine);
+
+        assertEquals(3, breakDownOutput.length);
+        assertEquals("You've reached Eastern Border : Can't go beyond this point", breakDownOutput[0]);
+        assertEquals("You've reached Eastern Border : Can't go beyond this point", breakDownOutput[1]);
+        assertEquals("Output:(X, Y , Direction)= (2,4,NORTH)", breakDownOutput[2]);
+
+    }
+
+    @Test
+    public void testWestBorder() throws IOException {
+        String[] args = {"CommandFiles/C13_WestBorderCheck.txt"};
+        String output;
+
+        try (ByteArrayOutputStream bOutput = new ByteArrayOutputStream()) {
+            System.setOut(new PrintStream(bOutput));
+            bOutput.flush();
+            Main.main(args);
+            output = bOutput.toString();
+        }
+
+        String newLine = System.getProperty("line.separator");
+        String[] breakDownOutput = output.split(newLine);
+
+        assertEquals(2, breakDownOutput.length);
+        assertEquals("You've reached Western Border : Can't go beyond this point", breakDownOutput[0]);
+        assertEquals("Output:(X, Y , Direction)= (2,0,NORTH)", breakDownOutput[1]);
+
+    }
 
 }
